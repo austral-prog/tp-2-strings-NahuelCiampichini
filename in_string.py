63ccc22e60cb45576a921e77b668072fdcd478fb
NahuelCiampichini <nnciampichini@mail.austral.edu.ar>
2026-03-27 10:15:28 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -3,8 +3,8 @@ def check_vowels():
     sin distinguir mayúsculas de minúsculas.
     """
     nombre = input("Nombre:").lower()
-    print(f"Contiene a: {"a" in nombre}")
-    print(f"Contiene e: {"e" in nombre}")
-    print(f"Contiene i: {"i" in nombre}")
-    print(f"Contiene o: {"o" in nombre}")
-    print(f"Contiene u: {"u" in nombre}")
+    print(f"Contiene a: {'a' in nombre}")
+    print(f"Contiene e: {'e' in nombre}")
+    print(f"Contiene i: {'i' in nombre}")
+    print(f"Contiene o: {'o' in nombre}")
+    print(f"Contiene u: {'u' in nombre}")
